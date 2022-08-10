13b7464ee46a7aa157e8d0b3d1e608d2fb50e39e
Caio Ramos <aramoscaio@gmail.com>
2022-08-10 20:17:33 -0300

    Guessing game

---
+++ guessing_game.py
@@ -0,0 +1,17 @@
+import random
+
+random_number = random.randint(1,20)
+user_number = 0
+
+
+while (user_number != random_number):
+	user_number = int(input('Insira seu chute! : '))
+	if user_number == random_number+1 or user_number == random_number-1:
+		print('That was SO CLOSE')
+	elif user_number == random_number+2 or user_number == random_number-2:
+		print('Almost got it')
+	elif user_number == random_number+3 or user_number == random_number-3:
+		print('Try again')
+	else:
+		print('Ice cold')
+print('NICE!! it was ' , random_number)
