934b17513e093ca5fe294fb7619f7ec155035274
Caio Ramos <aramoscaio@gmail.com>
2022-08-10 20:45:34 -0300

    Add minor changes

--- guessing_game.py
+++ guessing_game.py
@@ -12,6 +12,6 @@ while (user_number != random_number):
 		print('Almost got it')
 	elif user_number == random_number+3 or user_number == random_number-3:
 		print('Try again')
-	else:
+	elif user_number > random_number+4 or user_number < random_number-4:
 		print('Ice cold')
 print('NICE!! it was ' , random_number)
