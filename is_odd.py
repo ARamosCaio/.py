b7a6e3804dbd214f0dc48ac66dc99aa672720b04
Caio Ramos <aramoscaio@gmail.com>
2022-09-03 20:14:38 -0300

    Add blueblue e rename master to main

--- is_odd.py
+++ is_odd.py
@@ -1,3 +1,3 @@
 def is_odd(x):
 	return 'is odd' if x%2!=0 else 'is even'
-print(is_odd(int(input("Insira o valor desejado: "))))
+print(is_odd(int(input("Insira o valor desejado de blueblueblue: "))))
